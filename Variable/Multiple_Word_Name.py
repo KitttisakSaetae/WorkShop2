e6d66d3259c53d98f36e87cd6b76c0cde3ec37cb
KittisakSaetae <kittisak.sae@ku.th>
2021-01-10 11:09:22 +0700

    add 3 file (ManyValue, Multiple_Word_Name.py, Name.py)

---
+++ Variable/Multiple_Word_Name.py
@@ -0,0 +1,11 @@
+# Camal case
+myVariableName = "Kittisak Saetae"
+
+# Pascal case
+MyVariableName = "Kittisak Saetae"
+
+# Snake case
+my_variable_name = "Kittisak Saetae"
+
+
+number_list = [1, 2, 3]
---
+++ Variable/Name.py
@@ -0,0 +1,13 @@
+myvar = "Leang"
+my_var = "Leang"
+_my_var_ = "Leang"
+myVar = "Leang"
+MYVAR = "Leang"
+myvar2 = "Leang"
+
+# ไม่สามารถประกาศได้
+2myvar = "Leang"
+my-var = "Leang"
+my var = "Leang"
+
+PI = 3.14
