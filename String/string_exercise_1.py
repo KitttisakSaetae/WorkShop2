d047a764a05fa53ef3b2b8ead11fd43e88a6e554
KittisakSaetae <kittisak.sae@ku.th>
2021-01-16 11:03:20 +0700

    add string exercise 1

---
+++ String/string_exercise_1.py
@@ -0,0 +1,12 @@
+string = "Python is one of the fastest-growing programming languages"
+print(len(string))
+print(string[0])
+print(string[21:28])
+print(string.strip())
+print(string.upper())
+print(string.lower())
+print(string.replace("r", "x"))
+
+age = 36
+txt = f"My name is John, and I am {age}"
+print("txt", txt)
