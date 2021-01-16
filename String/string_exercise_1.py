459278982fce0de5cdbc40ab4d465cec607f42ac
KittisakSaetae <kittisak.sae@ku.th>
2021-01-16 11:09:37 +0700

    no message

--- String/string_exercise_1.py
+++ String/string_exercise_1.py
@@ -2,7 +2,7 @@ string = "Python is one of the fastest-growing programming languages"
 print(len(string))
 print(string[0])
 print(string[21:28])
-print(string.strip())
+print(string.replace(" ", ""))
 print(string.upper())
 print(string.lower())
 print(string.replace("r", "x"))
