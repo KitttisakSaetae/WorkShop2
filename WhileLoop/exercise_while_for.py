3bde83a1d62b7ea004e625631c47b0a70581e0de
KittisakSaetae <kittisak.sae@ku.th>
2021-02-06 20:09:14 +0700

    delete exercise_while.py

--- WhileLoop/exercise_while_for.py
+++
@@ -1,6 +0,0 @@
-for number in range(2, 13):
-    print("[ ", number, " ]")
-    for mutiple in range(1, 13):
-        print(number, " * ", mutiple, " = ", number * mutiple)
-    print("---------------------")
-    print()
