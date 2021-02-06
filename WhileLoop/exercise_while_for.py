1154f15f1d7b7323ab3a39c39f545d171e7dc168
KittisakSaetae <kittisak.sae@ku.th>
2021-02-06 19:53:12 +0700

    add exercise_while_for.py

--- WhileLoop/exercise_while_for.py
+++ WhileLoop/exercise_while_for.py
@@ -1,5 +1,6 @@
 for number in range(2, 13):
-    print()
     print("[ ", number, " ]")
     for mutiple in range(1, 13):
         print(number, " * ", mutiple, " = ", number * mutiple)
+    print("---------------------")
+    print()
